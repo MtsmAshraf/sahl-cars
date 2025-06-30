af1384a6f5450c99442bf2e291f7a897800c1b94
moatasim <mo32000a@gmail.com>
2025-06-30 22:40:26 +0300

    hero style edit

--- src/component/Hero/Hero.tsx
+++ src/component/Hero/Hero.tsx
@@ -4,9 +4,15 @@ import styles from "./hero.module.css"
 import { useTranslations } from 'next-intl'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
-// import Image from 'next/image'
-// import heroImg from "../../../public/images/hero.png"
-const Hero = () => {
+import Image from 'next/image'
+import heroImg from "../../../public/images/hero.png"
+
+
+const Hero = ({
+  lo
+} : {
+  lo: string
+}) => {
     const t = useTranslations("HomePage")
    useGSAP(() => {
     gsap.from(".stagger-text", {
@@ -25,39 +31,41 @@ const Hero = () => {
     })
      }, [])
   return (
-    <section className={styles.hero} id='hero'>
-        <div className="container">
-            <h2 className='stagger-text'>
-                {
-                    t("Hero.slogan")
-                }
-            </h2>
-            <p className='stagger-text'>
-                {
-                    t("Hero.p1")
-                }
-            </p>
-            <p className='stagger-text'>
-                {
-                    t("Hero.p2")
-                }
-            </p>
-            <div className={styles.btns + " " + "stagger-text"}>
-                <a href="#contact">
+    <section className={lo === "ar" ? styles.hero + " " + styles.ar : styles.hero} id='hero'>
+        <div>
+            <div className={styles.text}>
+                <h2 className='stagger-text'>
                     {
-                        t("Hero.btns.b1")
+                        t("Hero.slogan")
                     }
-                </a>
-                <a href="https://wa.me/+966505638988" target='_blank'>
+                </h2>
+                <p className='stagger-text'>
                     {
-                        t("Hero.btns.b2")
+                        t("Hero.p1")
                     }
-                </a>
+                </p>
+                <p className='stagger-text'>
+                    {
+                        t("Hero.p2")
+                    }
+                </p>
+                <div className={styles.btns + " " + "stagger-text"}>
+                    <a href="#contact">
+                        {
+                            t("Hero.btns.b1")
+                        }
+                    </a>
+                    <a href="https://wa.me/+966505638988" target='_blank'>
+                        {
+                            t("Hero.btns.b2")
+                        }
+                    </a>
+                </div>
+            </div>
+            <div className={styles.img}>
+                <Image src={heroImg} alt='Hero car'></Image>
             </div>
         </div>
-        {/* <div className={styles.img}>
-            <Image src={heroImg} alt='Hero car'></Image>
-        </div> */}
     </section>
   )
 }
