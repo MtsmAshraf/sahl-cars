752d6941ba90ed5c2e7a5f6db22b0bc1615a1f86
moatasim <mo32000a@gmail.com>
2025-07-08 18:29:01 +0300

    added locations

--- src/component/Contact/Contact.tsx
+++ src/component/Contact/Contact.tsx
@@ -11,13 +11,31 @@ const Contact = ({
   const t = useTranslations("HomePage.Contact")
   return (
     <section id='contact' className={styles.contact}>
-        <MainHeading>
-          {
-            t("heading")
-          }
-        </MainHeading>
+        
         <div className="container">
-          <WhatsAppForm lo={lo}/>
+          <div>
+              
+            <MainHeading>
+            {
+              t("heading")
+            }
+            </MainHeading>
+              <div className={styles.form}>
+                <WhatsAppForm lo={lo}/>
+            </div>
+          </div>
+        <div>
+            
+            <MainHeading>
+              {/* {
+                t("heading")
+              } */}
+              أين تجدنا؟
+              </MainHeading>
+            <div className={styles.location}>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1066.5405006360738!2d39.25722663730846!3d21.43282766257138!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1751987610555!5m2!1sen!2seg" width="600" height="450" style={{ border: "0" }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
         </div>
     </section>
   )
