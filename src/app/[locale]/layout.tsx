7035cfe0a505a7d77e1cdc52a7a0a47cfa1e3c38
moatasim <mo32000a@gmail.com>
2025-07-12 23:00:35 +0300

    tag manager for sahlcars.com

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -87,8 +87,25 @@ export default async function RootLayout({
             gtag('config', 'G-H1RN4HNPTY');
           `}
         </Script>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-59JW47HK');
+          `}
+        </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-59JW47HK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider>
           <Header lo={locale}></Header>
           {children}
