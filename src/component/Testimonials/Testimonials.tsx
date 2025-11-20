c9558c3df49c8b15f6bf8f126b7c62d1673c866d
moatasim <mo32000a@gmail.com>
2025-11-20 11:22:59 +0200

    added testimonials err fx

--- src/component/Testimonials/Testimonials.tsx
+++ src/component/Testimonials/Testimonials.tsx
@@ -1,5 +1,4 @@
-"use client"
-import React, { useRef } from 'react'
+import React from 'react'
 import styles from "./testimonials.module.css"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
